5005149748b03fff267153582e1e8e3cab5c28a0
Md Iqbal Hossain <mdiqbalhossain20775@gmail.com>
2023-11-30 20:21:42 +0600

    SOLVE PROBLEM

--- untitled/src/BEGINNER/BEE1008.java
+++ untitled/src/BEGINNER/BEE1008.java
@@ -1,4 +1,16 @@
 package BEGINNER;
 
+import java.io.IOException;
+import java.util.Scanner;
+
 public class BEE1008 {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int employeeNumber = sc.nextInt();
+        int workedHours = sc.nextInt();
+        double amountPerHour = sc.nextDouble();
+        System.out.println("NUMBER = " + employeeNumber);
+        System.out.printf("SALARY = U$ %.2f\n", workedHours * amountPerHour);
+        sc.close();
+    }
 }
