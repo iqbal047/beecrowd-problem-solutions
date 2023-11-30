5da30a7c62781799d00bd37f8b76722d58eccddd
Md Iqbal Hossain <mdiqbalhossain20775@gmail.com>
2023-11-30 20:09:11 +0600

    SOLVE PROBLEM

--- untitled/src/BEGINNER/BEE1006.java
+++ untitled/src/BEGINNER/BEE1006.java
@@ -1,4 +1,16 @@
 package BEGINNER;
 
+import java.io.IOException;
+import java.util.Scanner;
+
 public class BEE1006 {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        double a = sc.nextDouble();
+        double b = sc.nextDouble();
+        double c = sc.nextDouble();
+        double media = (a * 2 + b * 3 + c * 5) / 10;
+        System.out.printf("MEDIA = %.1f\n", media);
+        sc.close();
+    }
 }
